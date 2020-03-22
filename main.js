416481d1fb8971943af41c90f15e0f43c11c4b68
yuriikhomych23 <yuriikhomych94@gmail.com>
2020-03-22 17:26:51 +0000

    Sample code

---
+++ main.js
@@ -0,0 +1,59 @@
+//FOREACH
+// let arr = [5, 6, 7, 8, 9];
+// let sum = 0;
+// arr.forEach(function(value) {
+//     sum += value;
+// });
+// console.log(sum);
+//.....................
+
+
+//MAP
+// let arr = [5, 6, 7, 8, 9];
+// let sqr = arr.map(val => val * val);
+// console.log(sqr)
+//..............................
+
+
+//EVERY
+// let arr = [{
+//     name: 'Ivan',
+//     country: 'Ukraine'
+// }, {
+//     name: 'Petro',
+//     country: 'Ukraine'
+// },{
+//     name: 'Miguel',
+//     country: 'Cuba'
+// }];
+// let country = arr.every(arr => arr.country == 'Ukraine');
+// console.log(country) // FALSE
+//................................
+
+
+
+//SOME
+// let arr = [{
+//     name: 'Ivan',
+//     country: 'Ukraine'
+// }, {
+//     name: 'Petro',
+//     country: 'Ukraine'
+// },{
+//     name: 'Miguel',
+//     country: 'Cuba'
+// }];
+// let country = arr.some(arr => arr.country == 'Cuba');
+// console.log(country); //TRUE
+
+
+
+
+
+//LAST TASK
+// let arr = [1, -2, 3, 0, 4, -5, 6, -11];
+
+// let filter = arr.filter(num => num > 0 );
+// let sqr = filter.map(num => num * num);
+// console.log(sqr);
+
